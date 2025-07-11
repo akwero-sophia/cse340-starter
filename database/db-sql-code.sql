234148b1546ec72747f0413a20cc2ab1c347425e
myself <akwerosophia@gmail.com>
2025-07-11 20:51:00 +0300

    US

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -37,7 +37,8 @@ ALTER TABLE IF EXISTS public.inventory
      ADD CONSTRAINT fk_classification FOREIGN KEY (classification_id)
 	 REFERENCES public.classification (classification_id) MATCH SIMPLE
 	 ON UPDATE CASCADE
-	 ON DELETE ON ACTION;
+	 ON DELETE NO ACTION;
+
 
 
 
