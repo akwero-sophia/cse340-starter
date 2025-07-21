920eff642fadd01fd3f62bc6b92f98e03999106d
myself <akwerosophia@gmail.com>
2025-07-21 20:10:48 +0300

    committing

--- routes/misc.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-
-router.get('/trigger-error', (req, res, next) => {
-  next(new Error('Intentional 500 Error for testing'));
-});
-
-module.exports = router;
